7b3e1d75df40a6ec3c329c10de50641c3726dd74
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-29 16:02:43 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,14 +18,15 @@ def literal_aoh
   # held in the constants
   literal_aoh = [{
     :name =>  "Don Gately", :occupation => "Live-in Staff", 
-    :name =>  "Joelle van Dyne", :occupation => "Radio Personality" }, 
-    :name =>  "Pat Monteseian", :occupation => "Staff" }, 
-    :name =>  "Kate Gompert", :occupation => "None" }, 
-    :name =>  "Bruce Green", :occupation => "Fan of Mildred" }
+    :name =>  "Joelle van Dyne", :occupation => "Radio Personality", 
+    :name =>  "Pat Monteseian", :occupation => "Staff", 
+    :name =>  "Kate Gompert", :occupation => "None", 
+    :name =>  "Bruce Green", :occupation => "Fan of Mildred"
     }]
 end
 
 def aoh_lookup(aoh, row, key)
+  literal_aoh[]
 end
 
 def aoh_update(aoh, row, key, new_value)
